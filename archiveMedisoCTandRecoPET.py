bf904b4ce30dd24cc9368cdf764647b24e7ecb1b
Alex Waters <EAlexWaters@users.noreply.github.com>
2022-01-28 15:36:01 -0600

    Update and rename ScreenDicomHeaders_Modality.py to archiveMedisoCTandRecoPET.py

--- archiveMedisoCTandRecoPET.py
+++ archiveMedisoCTandRecoPET.py
@@ -10,11 +10,13 @@ It assumes data has been copied off the drive (not exported via a DICOM server i
 and is structured as a single parent folder (the source folder) containing numerous subfolders,
 each of which contains all the DICOM files for a single study. 
 
-The script iterates over the subfolders, exports a parameter log listing study parameters as 
-well as parameters for each individual scan, and optionally creates a .7z archive of each 
-subfolder with a human-readable name.
+The script iterates over the subfolders, exports a parameter log in .csv format listing study 
+parameters as well as parameters for each individual scan, and optionally creates a .7z archive 
+of each subfolder with a human-readable name.
 
-The script is designed to be used to archive data for cold storage.
+The script is designed to be used to archive unreconstructed and reconstructed CT data, as well as
+reconstructed PET data, for cold storage. Unreconstructed PET data is stored separately and will be 
+addressed in a separate script.
 
 Inputs: 
     src_dir:  directory containing subdirectories of unsorted DICOM files
